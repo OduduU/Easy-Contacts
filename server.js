23092b627e9a5f785001a8dbc9d5851ec9c4f825
OduduU <49434847+OduduU@users.noreply.github.com>
2020-04-09 03:52:33 +0100

    Prepare for deployment on heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const contactsRoute = require('./routes/contacts');
-const usersRoute = require('./routes/users');
-const authRoute = require('./routes/auth');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const contactsRoute = require("./routes/contacts");
+const usersRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect Database
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -14,10 +14,19 @@ const app = express();
 app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/users', usersRoute);
-app.use('/api/contacts', contactsRoute);
-app.use('/api/auth', authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/contacts", contactsRoute);
+app.use("/api/auth", authRoute);
 
+//  Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
 
 const PORT = process.env.PORT || 5000;
 
